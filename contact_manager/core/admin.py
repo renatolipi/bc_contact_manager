9c214a99c103cfc4f894f310e7ab48fd1606c5db
Renato Lipi <renatolipi@gmail.com>
2019-08-27 07:22:38 -0300

    add admin interface

--- contact_manager/core/admin.py
+++ contact_manager/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, LastContactTalk
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birthday', )
+    search_fields = ['name', ]
+
+
+class LastContactTalkAdmin(admin.ModelAdmin):
+    list_display = ('last_contact_date', 'person', )
+    list_filter = ('person__name', )
+    search_fields = ['person__name', ]
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(LastContactTalk, LastContactTalkAdmin)
--- contact_manager/core/models.py
+++ contact_manager/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    how_did_you_first_meet = models.TextField()
+    when_did_you_first_meet = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class LastContactTalk(models.Model):
+    last_contact_date = models.DateField()
+    subject = models.TextField()
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.last_contact_date}'
+
